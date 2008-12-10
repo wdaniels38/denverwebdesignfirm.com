5b1c103ebc377741f39389ae03916dd1e393264c
wdaniels38 <will@mindworxmedia.com>
2008-12-09 19:08:50 -0700

    added rails app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+	def index
+		@title = "Denver Web Design - Colorado Web Design - Denver Web Site Design"
+		@description = "Denver Web Design Firm, a Mindworx Media division, was created to offer Denver, Colorado with awesome web
+		design and web development. Our work is truly spectacular."
+		@keywords = "denver web design, denver web development, colorado web design, colorado web development"
+	end
+end
